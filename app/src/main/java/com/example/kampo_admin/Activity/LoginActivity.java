812027226b29a6a4fde23a4651379114bb2094c0
Gautam Sharma <gautamsharma88540@gmail.com>
2021-07-16 21:18:18 +0530

    Update Project

---
+++ app/src/main/java/com/example/kampo_admin/Activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.kampo_admin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/kampo_admin/Activity/MainActivity.java
+++ app/src/main/java/com/example/kampo_admin/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.kampo_admin;
+package com.example.kampo_admin.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.kampo_admin.R;
 
 public class MainActivity extends AppCompatActivity {
 
