4badccbfd1b4faea4cb263411c93b7f85983eb77
Gaurav Verma <70060764+gauravv0312@users.noreply.github.com>
2021-07-17 13:05:35 +0530

    update

--- app/src/main/java/com/example/kampo_admin/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/kampo_admin/Fragments/LoginFragment.java
@@ -1,66 +1,86 @@
 package com.example.kampo_admin.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.kampo_admin.Activity.LoaderActivity;
+import com.example.kampo_admin.Activity.RegistrationActivity;
 import com.example.kampo_admin.R;
+import com.example.kampo_admin.databinding.ActivityLoginBinding;
+import com.example.kampo_admin.databinding.FragmentLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
 public class LoginFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    FragmentLoginBinding binding;
+    //FireBase instance
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    //Email pattern
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        //Calling Function Login When user click on button
+        binding.loginButton.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(binding.userEmailAddressText.getText()).toString().trim();
+            String password = Objects.requireNonNull(binding.loginPasswordText.getText()).toString().trim();
+            logIn(email, password);
+
+        });
+
+        //Sending User To the Registration Activity
+        binding.signUpText.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), RegistrationActivity.class);
+            startActivity(intent);
+        });
+        return binding.getRoot();
+    }
+
+    private void logIn(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
+            binding.userEmailAddressText.setError("Email is required");
+        } else if (!email.trim().matches(emailPattern)) {
+            binding.userEmailAddressText.setError("Email is Not Valid");
+        } else if (TextUtils.isEmpty(password)) {
+            binding.loginPasswordText.setError("Password is required");
+        } else {
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(getActivity(), task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUi(user);
+
+                        } else {
+                            Toast.makeText(getContext(), "May Be Your Email or Password Wrong!", Toast.LENGTH_SHORT).show();
+                            updateUi(null);
+
+                        }
+                    });
+        }
+    }
+
+    private void updateUi(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(getContext(), LoaderActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getContext(), "Try Again!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
--- app/src/main/java/com/example/kampo_admin/Fragments/RegistrationFragment.java
+++ app/src/main/java/com/example/kampo_admin/Fragments/RegistrationFragment.java
@@ -1,66 +1,122 @@
 package com.example.kampo_admin.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.example.kampo_admin.R;
+import com.example.kampo_admin.Activity.LoaderActivity;
+import com.example.kampo_admin.Activity.LoginActivity;
+import com.example.kampo_admin.Activity.MainActivity;
+import com.example.kampo_admin.databinding.FragmentRegistrationBinding;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegistrationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
 public class RegistrationFragment extends Fragment {
+FragmentRegistrationBinding binding;
+//Creating Firebase Instance
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final FirebaseFirestore fStore = FirebaseFirestore.getInstance();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public RegistrationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrationFragment newInstance(String param1, String param2) {
-        RegistrationFragment fragment = new RegistrationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+//    Creating Variable
+      String userFullName,email,mobileNumber,specialization;
+      String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+    binding=FragmentRegistrationBinding.inflate(inflater,container,false);
+
+    //calling Signup to login Screen
+    binding.signUpButton.setOnClickListener(v -> signup());
+        //Sending User To login Screen
+        binding.logInText.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+        });
+    return binding.getRoot();
     }
+
+    private void signup() {
+        userFullName = Objects.requireNonNull(binding.fullNameText.getText()).toString().trim();
+        email = Objects.requireNonNull(binding.userEmailAddressText.getText()).toString().trim();
+        mobileNumber=Objects.requireNonNull(binding.numberText.getText()).toString().trim();
+        specialization=Objects.requireNonNull(binding.specialistText.getText()).toString().trim();
+        String password = Objects.requireNonNull(binding.loginPasswordText.getText()).toString();
+        if (TextUtils.isEmpty(userFullName)) {
+            binding.fullNameText.setError("Full Name is Required");
+        }
+        else if(TextUtils.isEmpty(email)){
+            binding.userEmailAddressText.setError("Email is required");
+        }
+        else if(!email.trim().matches(emailPattern)){
+            binding.userEmailAddressText.setError("Email is Not Valid");
+        }
+        else if(TextUtils.isEmpty(password)){
+            binding.loginPasswordText.setError("Password is required");
+        }
+        else if(password.length() < 6){
+            binding.loginPasswordText.setError("Password Must be equal to 6 character or greater than 6");
+        }
+        else if (TextUtils.isEmpty(mobileNumber)){
+            binding.numberText.setError("Mobile Number is Required");
+        }
+        else if (TextUtils.isEmpty(specialization)){
+            binding.specialistText.setError("Specialization is Required");
+        }
+        else {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task ->{
+                if (task.isSuccessful())
+                {
+                    FirebaseUser user= mAuth.getCurrentUser();
+                    DocumentReference documentReference = fStore.collection("Workers").document(Objects.requireNonNull(mAuth.getUid()));
+                    Map<String, Object> workers =new HashMap<>();
+                    workers.put("FullName",userFullName);
+                    workers.put("Email",email);
+                    workers.put("PhoneNumber",mobileNumber);
+                    workers.put("Specialization",specialization);
+                    documentReference.set(workers).addOnCompleteListener(unused -> updateUI(user));
+                }
+                else {
+                    Toast.makeText(getContext(), "Error ! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } );
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null)
+        {
+            Intent intent = new Intent(getContext(), LoaderActivity.class);
+            intent.putExtra("Name",userFullName);
+            intent.putExtra("Email",email);
+            startActivity(intent);
+        }
+
+        else {
+            Toast.makeText(getContext(), "Try Again Some Time!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
